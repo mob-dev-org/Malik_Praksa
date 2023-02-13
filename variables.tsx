83eb1bec18df75a24a100478607c96fe7e34bc9c
Malik Porca <malik.porca.22@size.ba>
2023-02-13 19:59:21 +0100

    variables

---
+++ variables.tsx
@@ -0,0 +1,8 @@
+let message;
+message="esselamualejkum";//store some text in variable
+
+let age=10, Name="Omar"//this is not recomended
+
+const place="Zenica" //constant
+
+// There are some reserved words and we can not use them for nameing variables like return or function...
