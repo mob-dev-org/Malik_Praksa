56f9ffb85d65504f3674c0f8b3437c35c200dda2
Malik Porca <malik.porca.22@size.ba>
2023-02-14 15:50:30 +0100

    fixing

--- variables.tsx
+++ variables.tsx
@@ -1,8 +1,8 @@
-let message;
+let message:string;
 message="esselamualejkum";//store some text in variable
 
-let age=10, Name="Omar"//this is not recomended
+let age=10, names="Omar"//this is not recomended
 
-const place="Zenica" //constant
+const city="Zenica" //constant
 
 // There are some reserved words and we can not use them for nameing variables like return or function...
