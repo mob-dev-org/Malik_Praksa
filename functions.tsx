ebfbfb3b9c4e102f316373120286b93313f71c07
Malik Porca <malik.porca.22@size.ba>
2023-02-14 07:42:45 +0100

    functions

---
+++ functions.tsx
@@ -0,0 +1,14 @@
+function showMessage (){
+    alert("this is message")//function will alert message
+}
+
+showMessage()//this will also alert same message, this is calling function
+
+
+function message (name:string, message:string){
+    alert(name + message)
+}
+
+
+message("maki", "how are you")//this two parameters will be sended to function message 
+
