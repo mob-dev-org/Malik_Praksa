32af1bfe97eb6bb22427caedac6707adbbe684a4
Malik Porca <malik.porca.22@size.ba>
2023-02-15 20:23:59 +0100

    fix

--- spredOperator.tsx
+++ spredOperator.tsx
@@ -1,5 +1,16 @@
-
-
-
 let numbers:number[] = [1,2,3,4,5]
-alert([...numbers])//it will alert all numbers from array
+alert(...numbers)//it will alert all numbers from array
+
+type People={
+    name:string,
+    age:number,
+    job:string
+}
+
+let people:People={
+    name:"Malik", 
+    age:23,
+    job:"police"
+}
+
+alert({...people})
